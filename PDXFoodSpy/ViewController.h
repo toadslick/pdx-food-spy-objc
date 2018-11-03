a365dfbc7890514355f104564102663f5ccc7fcb
James Childress <jameschildress@toadbook-air.local>
2018-11-03 16:07:15 -0700

    Initial Commit

---
+++ PDXFoodSpy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDXFoodSpy
+//
+//  Created by James Childress on 11/3/18.
+//  Copyright © 2018 James Childress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PDXFoodSpy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PDXFoodSpy
+//
+//  Created by James Childress on 11/3/18.
+//  Copyright © 2018 James Childress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
