29ca70429351f1db192bb490dd055e8740e7b273
James Childress <jchildress@cvent.com>
2018-11-08 20:15:25 -0800

    wip: crash at longValue

--- PDXFoodSpy/PDX Restaurant Inspections API/SearchNearCoordinate.m
+++ PDXFoodSpy/PDX Restaurant Inspections API/SearchNearCoordinate.m
@@ -37,7 +37,8 @@
 
 
 - (void)jsonFetcher:(JSONFetcher *)fetcher didReceiveJSON:(NSDictionary *)json {
-    NSLog(@"RESTAURANT SEARCH SUCCESS: %@", json);
+    NSArray<SearchResult *> *results = [self buildSearchResults:json];
+    NSLog(@"RESTAURANT SEARCH SUCCESS: %@", results);
 }
 
 - (void)jsonFetcher:(JSONFetcher *)fetcher didFailWithError:(NSError *)error {
@@ -46,19 +47,38 @@
 
 - (NSArray<SearchResult *> *)buildSearchResults:(NSDictionary *)json {
     NSArray<NSDictionary *> *dicts = [json objectForKey:@"results"];
-    NSArray<SearchResult *> *results = @[];
+    NSDictionary<NSString *, SearchResult *> *latestResults = @{};
     
     // The "results" object will be nil if the JSON response returned with zero results found.
     if (dicts) {
         [dicts enumerateObjectsUsingBlock:^(NSDictionary * _Nonnull dict, NSUInteger idx, BOOL * _Nonnull stop) {
+            
             // Only keep results for which the type is "FoodSvcSemi".
             NSString *type = [dict objectForKey:@"type"];
             if ([type isEqualToString:@"FoodSvcSemi"]) {
                 SearchResult *result = [SearchResult initFromJSONDictionary:dict];
+                
+                // Only keep the latest result for each restaurant.
+                // Store each result in a dictionary according to the restaurant ID.
+                SearchResult *previousResult = [latestResults objectForKey:result.restaurantID];
+                
+                // Use this result if it has a later inspection date than the previous result.
+                if (previousResult) {
+                    if ([[result.date laterDate:previousResult.date] isEqualToDate:result.date]) {
+                        [dict setValue:result forKey:result.restaurantID];
+                    }
+
+                // If a result doesn't exist for the restaurant ID, use this result.
+                } else {
+                    [dict setValue:result forKey:result.restaurantID];
+                }
             }
         }];
+        
+        return [latestResults allValues];
+    } else {
+        return @[];
     }
-    return results;
 }
 
 
--- PDXFoodSpy/PDX Restaurant Inspections API/SearchResult.m
+++ PDXFoodSpy/PDX Restaurant Inspections API/SearchResult.m
@@ -3,6 +3,8 @@
 @implementation SearchResult
 
 + (SearchResult *)initFromJSONDictionary:(NSDictionary *)dict {
+    NSLog(@"RESULT: %@", dict);
+    
     SearchResult *result = [SearchResult new];
     NSDictionary *address = [dict objectForKey:@"address"];
     NSDictionary *location = [dict objectForKey:@"location"];
@@ -11,12 +13,14 @@
     dateFormatter.dateFormat = @"yyyy-MM-dd";
     
     result.name = [dict objectForKey:@"name"];
-    result.address = [address objectForKey:@"address"];
+    result.address = [address objectForKey:@"street"];
     result.zip = [address objectForKey:@"zip"];
     result.inspectionID = [dict objectForKey:@"inspection_number"];
     result.restaurantID = [dict objectForKey:@"restaurant_id"];
     result.score = [[dict objectForKey:@"score"] integerValue];
     result.date = [dateFormatter dateFromString:[dict objectForKey:@"date"]];
+    
+    NSLog(@"COORDINATE: %@, %@", [location objectForKey:@"Latitude"], [location objectForKey:@"Longitude"]);
     result.coordinate = CLLocationCoordinate2DMake(
         [[location objectForKey:@"Latitude"] longValue],
         [[location objectForKey:@"Longitude"] longValue]);
