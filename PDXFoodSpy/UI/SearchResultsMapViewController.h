9ba09a6a37dd058d2351ffc637deebb029e2ccd7
James Childress <jameschildress@toadbook-air.local>
2018-11-11 22:18:17 -0800

    pins for map view

--- PDXFoodSpy/UI/SearchResultsMapViewController.h
+++ PDXFoodSpy/UI/SearchResultsMapViewController.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
-#import <MapKit/MKMapView.h>
+#import <MapKit/MapKit.h>
+#import "SearchResultsTabBarController.h"
+#import "SearchResult.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
--- PDXFoodSpy/UI/SearchResultsMapViewController.m
+++ PDXFoodSpy/UI/SearchResultsMapViewController.m
@@ -4,10 +4,22 @@
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @end
 
-@implementation SearchResultsMapViewController
+@implementation SearchResultsMapViewController {
+    NSArray<SearchResult *> *results;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    SearchResultsTabBarController *parent = (SearchResultsTabBarController *)[self parentViewController];
+    results = parent.results;
+    
+    // Add pins to the map view.
+    [self.mapView removeAnnotations:[self.mapView annotations]];
+    [results enumerateObjectsUsingBlock:^(SearchResult * _Nonnull result, NSUInteger idx, BOOL * _Nonnull stop) {
+        MKPointAnnotation *point = [MKPointAnnotation new];
+        point.coordinate = result.coordinate;
+        [self.mapView addAnnotation:point];
+    }];
 }
 
 @end
