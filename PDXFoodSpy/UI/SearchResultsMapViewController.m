af5538d59ce198aedf295ce3b548a77c7d2cafd3
James Childress <jameschildress@toadbook-air.local>
2018-11-17 15:27:05 -0800

    map callout

--- PDXFoodSpy/UI/SearchResultsMapViewController.m
+++ PDXFoodSpy/UI/SearchResultsMapViewController.m
@@ -1,17 +1,15 @@
 #import "SearchResultsMapViewController.h"
+#import "SearchResultTableViewCell.h"
 
 @interface SearchResultsMapViewController ()
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @end
 
-@implementation SearchResultsMapViewController {
-    NSArray<SearchResult *> *results;
-}
+@implementation SearchResultsMapViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSArray<SearchResult *> *results = ((SearchResultsTabBarController *)[self parentViewController]).results;
-    
     self.mapView.delegate = self;
 
     // Add pins to the map view.
@@ -20,9 +18,11 @@
     
     // Adjust the map's visible region to fit the added pins.
     [self.mapView showAnnotations:results animated:false];
-    
 }
 
+// Render the map marker for each search result.
+// Use the inspection score as the reuse identifier because the marker's customization,
+// such as tintColor and glyphText, are determined by the score.
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
     SearchResult *result = (SearchResult *)annotation;
     MKAnnotationView *reuseMarker = [mapView dequeueReusableAnnotationViewWithIdentifier:[result scoreString]];
@@ -32,10 +32,14 @@
         MKMarkerAnnotationView *marker = [[MKMarkerAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:[result scoreString]];
         marker.markerTintColor = [result scoreColor];
         marker.glyphText = [result scoreString];
+        marker.canShowCallout = YES;
+        marker.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
         return marker;
     }
 }
 
-
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control {
+    
+}
 
 @end
