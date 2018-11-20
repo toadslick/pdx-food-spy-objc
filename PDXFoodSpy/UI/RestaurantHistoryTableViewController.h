14e0448382e0fb4f8d3d95c980cc1f72dbd2bb56
James Childress <jameschildress@toadbook-air.local>
2018-11-20 04:46:51 -0800

    dynamic fonts styles

---
+++ PDXFoodSpy/UI/RestaurantHistoryTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  RestaurantHistoryTableViewController.h
+//  PDXFoodSpy
+//
+//  Created by James Childress on 11/20/18.
+//  Copyright © 2018 James Childress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RestaurantHistoryTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
