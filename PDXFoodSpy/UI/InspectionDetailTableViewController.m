40895ca25f03a20fd1f996128ff2029eb67e18e4
James Childress <jameschildress@toadbook-air.local>
2018-11-21 06:39:42 -0800

    progress on detail view

---
+++ PDXFoodSpy/UI/InspectionDetailTableViewController.m
@@ -0,0 +1,21 @@
+#import "InspectionDetailTableViewController.h"
+
+@interface InspectionDetailTableViewController ()
+
+@end
+
+@implementation InspectionDetailTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+@end
---
+++ PDXFoodSpy/UI/InspectionDetailViewController.m
@@ -0,0 +1,13 @@
+#import "InspectionDetailViewController.h"
+
+@interface InspectionDetailViewController ()
+
+@end
+
+@implementation InspectionDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
