78d06891e37fc6aa1a201e19ef91fcf94c5cd393
James Childress <jchildress@cvent.com>
2018-11-22 03:51:59 -0800

    render detail comments

--- PDXFoodSpy/UI/InspectionDetailTableViewController.m
+++ PDXFoodSpy/UI/InspectionDetailTableViewController.m
@@ -11,11 +11,42 @@
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 0;
+    if (self.violations) {
+        return self.violations.count;
+    } else {
+        return 0;
+    }
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
+//    InspectionViolation *violation = [self.violations objectAtIndex:section];
+    return 4;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    InspectionViolation *violation = [self.violations objectAtIndex:section];
+    return violation.lawCode;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    InspectionViolation *violation = [self.violations objectAtIndex:indexPath.section];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"detailCell"];
+
+    switch (indexPath.row) {
+        case 0:
+            cell.textLabel.text = violation.violationText;
+            break;
+        case 1:
+            cell.textLabel.text = violation.violationComments;
+            break;
+        case 2:
+            cell.textLabel.text = violation.correctiveText;
+            break;
+        case 3:
+            cell.textLabel.text = violation.correctiveComments;
+            break;
+    }
+    return cell;
 }
 
 @end
